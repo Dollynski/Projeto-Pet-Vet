78865117b69c53eb4526bb920246e63d0961f139
TechGui <guilhermedrsilva7@gmail.com>
2024-11-11 00:00:45 -0300

    Atualiza CRUD Completo

--- src/app/consultas/page.tsx
+++ src/app/consultas/page.tsx
@@ -36,7 +36,7 @@ export default function Consultas() {
           throw new Error(`Error: ${response.status}`);
         }
         const dados = await response.json();
-        setConsultas([dados]);
+        setConsultas(dados);
       } catch (error) {
         console.error("Erro ao buscar consultas:", error);
       }
@@ -61,9 +61,17 @@ export default function Consultas() {
 
     buscaDados();
     buscaTutores();
-    buscaDados();
     buscaVeterinarios();
     buscaPets();
+
+    const intervalId = setInterval(() => {
+      buscaDados();
+      buscaTutores();
+      buscaVeterinarios();
+      buscaPets();
+    }, 5000); // Atualiza a cada 5 segundos
+
+    return () => clearInterval(intervalId); // Limpa o intervalo quando o componente é desmontado
   }, [veterinario.id]);
 
   function dataDMA(data: string) {
@@ -208,37 +216,144 @@ const exibirFormularioCadastroConsulta = () => {
       </div>
       </div>
       <h1 className="flex justify-center align-center mb-6 mt-4 text-3xl font-extrabold leading-none tracking-tight text-black md:text-4xl lg:text-5xl dark:text-white">
-        Listagem de <span className="underline underline-offset-3 decoration-8 decoration-[#67AFB3] dark:decoration-[#67AFB3]">Minhas consultas</span>
+      <span className="underline underline-offset-3 decoration-8 decoration-[#67AFB3] dark:decoration-[#67AFB3]"> Minhas consultas pendentes</span>
       </h1>
 
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" className="px-6 py-3">ID do Pet</th>
+            <th scope="col" className="px-6 py-3">Nome do Pet</th>
             <th scope="col" className="px-6 py-3">Tutor</th>
+            <th scope="col" className="px-6 py-3">Data</th>
             <th scope="col" className="px-6 py-3">Foto</th>
             <th scope="col" className="px-6 py-3">Descrição</th>
             <th scope="col" className="px-6 py-3">Ações</th>
           </tr>
         </thead>
         <tbody>
-          {consultas.map(consulta => (
+          {consultas.filter(consulta => consulta.status === "AGUARDANDO").map(consulta => (
             <tr key={consulta.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
               <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                {consulta.petId}
+                {consulta.pet.nome}
               </td>
-              <td className="px-6 py-4">{consulta.tutor.nome}</td>
+              <td className="px-6 py-4">{consulta.tutor ? consulta.tutor.nome : 'Nome do tutor não disponível'}</td>
               <td className="px-6 py-4">
-                <img src={consulta.pet.foto} className="fotoPet" alt="Foto do pet" />
+                <p>{dataDMA(consulta.data)}</p>
+                <p><i>Horário: {new Date(consulta.data).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</i></p>
+              </td>
+              <td className="px-6 py-4">
+              <img src={consulta.pet?.foto || 'foto_default.jpg'} alt="Foto do Pet" className="w-10 h-10 rounded-full" />
               </td>
               <td className="px-6 py-4">
                 <p><b>{consulta.descricao}</b></p>
                 <p><i>Consultado em: {dataDMA(consulta.data)}</i></p>
               </td>
-              <td className="px-6 py-4">
-                <Link href="/veterinario/detalheConsulta">
-                  <button className="font-medium text-[#67AFB3] dark:text-blue-500">Detalhes</button>
-                </Link>
+                <td className="px-6 py-4 flex gap-2">
+                <button
+                onClick={() => {
+                  (async () => {
+                  const response = await fetch(`${process.env.NEXT_PUBLIC_URL_API}/consultas/${consulta.id}/confirmar`, {
+                  method: 'PATCH',
+                  headers: { 'Content-Type': 'application/json' },
+                  });
+                  if (response.ok) {
+                  const updatedConsulta = await response.json();
+                  setConsultas(prevConsultas =>
+                    prevConsultas.map(c => (c.id === updatedConsulta.id ? updatedConsulta : c))
+                  );
+                  Swal.fire({
+                    icon: 'success',
+                    title: '<h2 style="color: #ffffff; font-size: 1.2rem;">Sucesso</h2>',
+                    text: 'Consulta confirmada com sucesso!',
+                    confirmButtonColor: '#A4D0C3',
+                    background: '#1F1F1F',
+                  });
+                  }
+                  })();
+                }}
+                className="text-green-500 hover:text-green-700"
+                >
+                <img src="./concluir.png" alt="Confirmar" className="w-6 h-6" />
+                </button>
+
+                <button
+                onClick={() => {
+                  (async () => {
+                    const response = await fetch(`${process.env.NEXT_PUBLIC_URL_API}/consultas/${consulta.id}/deletar`, {
+                    method: 'DELETE',
+                    headers: { 'Content-Type': 'application/json' },
+                    });
+                    if (response.ok) {
+                    setConsultas(prevConsultas => prevConsultas.filter(c => c.id !== consulta.id));
+                    Swal.fire({
+                    icon: 'success',
+                    title: '<h2 style="color: #ffffff; font-size: 1.2rem;">Sucesso</h2>',
+                    text: 'Consulta deletada com sucesso!',
+                    confirmButtonColor: '#A4D0C3',
+                    background: '#1F1F1F',
+                    });
+                    }
+                  })();
+                }}
+                className="text-red-500 hover:text-red-700"
+                >
+                <img src="./cancelar.png" alt="Deletar" className="w-6 h-6" />
+                </button>
+
+                <button
+                onClick={async () => {
+                  const { value: newDate } = await Swal.fire({
+                    title: '<h2 style="color: #ffffff; font-size: 1.2rem;">Reagendamento de consulta</h2>',
+                    html: `
+                      <label style="color: #ffffff; font-size: 0.8rem;">Nova Data da Consulta</label>
+                      <input type="date" id="newDate" class="swal2-input" style="background-color: #333333; color: #ffffff; font-size: 0.8rem;">
+                    `,
+                    confirmButtonText: 'REAGENDAR',
+                    confirmButtonColor: '#A4D0C3',
+                    background: '#1F1F1F',
+                    preConfirm: () => {
+                      const newDate = (document.getElementById('newDate') as HTMLInputElement).value;
+                      if (!newDate) {
+                        Swal.showValidationMessage('Por favor, selecione uma nova data');
+                      }
+                      return newDate;
+                    }
+                  });
+
+                  if (newDate) {
+                    const response = await fetch(`${process.env.NEXT_PUBLIC_URL_API}/consultas/${consulta.id}/alterar`, {
+                      method: 'PATCH',
+                      headers: { 'Content-Type': 'application/json' },
+                      body: JSON.stringify({ data: newDate })
+                    });
+
+                    if (response.ok) {
+                      const updatedConsulta = await response.json();
+                      setConsultas(prevConsultas =>
+                        prevConsultas.map(c => (c.id === updatedConsulta.id ? updatedConsulta : c))
+                      );
+                      Swal.fire({
+                        icon: 'success',
+                        title: '<h2 style="color: #ffffff; font-size: 1.2rem;">Sucesso</h2>',
+                        text: 'Consulta reagendada com sucesso!',
+                        confirmButtonColor: '#A4D0C3',
+                        background: '#1F1F1F',
+                      });
+                    } else {
+                      Swal.fire({
+                        icon: 'error',
+                        title: '<h2 style="color: #ffffff; font-size: 1.2rem;">Erro</h2>',
+                        text: 'Erro ao reagendar consulta',
+                        confirmButtonColor: '#A4D0C3',
+                        background: '#1F1F1F',
+                      });
+                    }
+                  }
+                }}
+                className="text-yellow-500 hover:text-yellow-700"
+                >
+                <img src="./editar.png" alt="Alterar" className="w-6 h-6" />
+                </button>
               </td>
             </tr>
           ))}
