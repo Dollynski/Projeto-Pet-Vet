2d577fd54b44420e8840eb3c7841f75b0a178370
Luis Felipe Jr <felipequineper@hotmail.com>
2024-12-02 19:46:34 -0300

    Alterações componente de pesquisa

--- src/components/InputPesquisa.tsx
+++ src/components/InputPesquisa.tsx
@@ -49,7 +49,7 @@ export function InputPesquisa({ setPets }: InputPesquisaProps) {
             type="search"
             id="default-search"
             className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="O que você está procurando? (modelo, marca, ano ou preço máximo)"
+            placeholder="Digite o que você está procurando (Nome, Raça, Espécie, Sexo)"
             required
             {...register("termo")}
           />
