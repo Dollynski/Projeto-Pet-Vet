f17b08330edfbdd174dc755aaa5161370447bb1f
TechGui <guilhermedrsilva7@gmail.com>
2024-12-08 17:53:08 -0300

    Adiciona Dashboard e Altera Detalhes Form Cadastro Pet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,27 @@ import { useEffect, useState } from "react";
 import { Toaster } from 'sonner'
 import { useVeterinarioStore } from "@/context/veterinario";
 import Link from "next/link";
+import { Chart } from "react-google-charts";
+
+interface ConsultaVeterinarioI {
+  veterinario: string;
+  num: number;
+}
+
+interface GeralDadosI {
+  consultas: number;
+  veterinarios: number;
+  pets: number;
+}
+
+type DataRow = [string, number, string];
+
 
 export default function Home() {
   const [pets, setPets] = useState<PetI[]>([])
   const [veterinario, setVeterinario] = useState<any>(null);
+  const [consultasVeterinario, setConsultasVeterinario] = useState<ConsultaVeterinarioI[]>([]);
+  const [dados, setDados] = useState<GeralDadosI>({} as GeralDadosI);
   const { logaVeterinario } = useVeterinarioStore()
 
   useEffect(() => {
@@ -33,11 +50,36 @@ export default function Home() {
       setPets(dados)
     }
     buscaDados()
+
+    async function getDadosGerais() {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_URL_API}/dashboard/gerais`);
+      const dados = await response.json();
+      setDados(dados);
+    }
+
+    async function getDadosGrafico() {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_URL_API}/dashboard/consultasVeterinario`);
+      const dados = await response.json();
+      setConsultasVeterinario(dados);
+    }
+    getDadosGerais();
+    getDadosGrafico();
   }, [])
+
 
   const listaPets = pets.map(pet => (
     <ItemPet data={pet} key={pet.id} />
   ))
+
+  const data: (["Veterinário", "Nº Consultas já confirmadas", { role: string }] | DataRow)[] = [
+    ["Veterinário", "Nº Consultas já confirmadas", { role: "style" }],
+  ];
+
+  const cores = ["red", "blue", "violet", "green", "gold", "cyan", "chocolate", "purple", "brown", "orangered"];
+  consultasVeterinario.forEach((consulta, index) => {
+    data.push([consulta.veterinario, Number(consulta.num), cores[index % 10]]);
+  });
+
 
   return (
     <main className="min-h-screen flex flex-col items-center bg-gray-100 w-[84%] ml-auto bg-[url('/pata.png')] bg-center bg-no-repeat bg-[length:56%]">
@@ -50,6 +92,47 @@ export default function Home() {
               </h1>
               <Toaster position="top-right" richColors />
             </div>
+
+            <section className="container mt-24 flex flex-col">
+
+              <div className="w-3/4 flex justify-between mx-auto mb-5">
+                <div className="border-blue-600 border rounded p-6 w-1/3 me-3">
+                  <span className="bg-blue-100 text-blue-800 text-xl text-center font-bold mx-auto block px-2.5 py-5 rounded dark:bg-blue-900 dark:text-blue-300">
+                    {dados.veterinarios}
+                  </span>
+                  <p className="font-bold mt-2 text-center">Nº Veterinários</p>
+                </div>
+                <div className="border-red-600 border rounded p-6 w-1/3 me-3">
+                  <span className="bg-red-100 text-red-800 text-xl text-center font-bold mx-auto block px-2.5 py-5 rounded dark:bg-red-900 dark:text-red-300">
+                    {dados.consultas}
+                  </span>
+                  <p className="font-bold mt-2 text-center">Nº Consultas Pendentes</p>
+                </div>
+                <div className="border-green-600 border rounded p-6 w-1/3">
+                  <span className="bg-green-100 text-green-800 text-xl text-center font-bold mx-auto block px-2.5 py-5 rounded dark:bg-green-900 dark:text-green-300">
+                    {dados.pets}
+                  </span>
+                  <p className="font-bold mt-2 text-center">Nº Pets no Sistema</p>
+                </div>
+              </div>
+
+                <h2 className="text-2xl font-bold mt-16 text-center">Gráfico: Nº de Consultas por Veterinário</h2>
+                <div className="p-4 rounded-lg flex justify-center">
+                <Chart
+                  chartType="AreaChart" // Ou "BarChart" para um gráfico de barras
+                  width="100%"
+                  height="280px"
+                  data={data}
+                  options={{
+                  legend: { position: "bottom", textStyle: { color: "#67AFB3" } },
+                  hAxis: { textStyle: { color: "#67AFB3" } },
+                  vAxis: { textStyle: { color: "#67AFB3" } },
+                  colors: ["#67AFB3"],
+                  }}
+                />
+                </div>
+            </section>
+
           </>
         ) : (
           <>
--- src/app/pets/page.tsx
+++ src/app/pets/page.tsx
@@ -96,18 +96,7 @@ export default function Home() {
       <p style="color: #bbbbbb; font-size: 0.8rem; margin-bottom: 15px;">
         Preencha os dados para cadastrar o pet:
       </p>
-      <div class="flex items-center justify-center w-full mb-4 mx-auto">
-        <label for="dropzone-file" class="flex flex-col bg-transparent  items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer hover:bg-gray-700">
-        <div class="flex flex-col items-center justify-center pt-5 pb-6">
-          <svg class="w-8 h-8 mb-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
-          </svg>
-          <p class="mb-2 text-sm text-gray-500"><span class="font-semibold">Clique para fazer upload</span> ou arraste e solte</p>
-          <p class="text-xs text-gray-500">SVG, PNG, JPG ou GIF (Res. MAX. 800x400px)</p>
-        </div>
-        <input id="dropzone-file" type="file" class="hidden" />
-        </label>
-      </div>
+
 
       <div style="display: flex; flex-direction: column; gap: 10px; justify-content: center; align-items: center;">
         <label style="color: #ffffff; font-size: 0.8rem;">Foto</label>
